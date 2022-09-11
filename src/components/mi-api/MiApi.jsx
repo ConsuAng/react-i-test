7a6e6921084642ce30d8ae16699acc1572c8c14f
Consue <c.anguitasalinas@gmail.com>
2022-09-11 18:45:23 -0300

    feat: header and content card added

---
+++ src/components/mi-api/MiApi.jsx
@@ -0,0 +1,97 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import logo from "../../assets/logo.svg";
+import Card from "../card/Card";
+import NotFound from "../not-found/NotFound";
+
+export default function MiApi() {
+  const [films, setFilms] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData() {
+    const url = "https://ghibliapi.herokuapp.com/films"
+    const res = await axios.get(url);
+    const { data } = await res;
+    const sortData = data.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      } if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    });
+
+    setFilms(sortData);
+  }
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const listFilms = !search
+    ? films
+    : films.filter((x) => x.title.toLowerCase().includes(search.toLocaleLowerCase()) || 
+      x.director.toLowerCase().includes(search.toLocaleLowerCase()) || 
+      x.release_date.includes(search));
+
+  return (
+    <>
+      <div className="w-full bg-[#cde7f4] h-24 fixed top-0 
+      flex flex-col items-center md:flex-row md:justify-between
+      ">
+        <img className="h-[54px] md:h-[96px]" 
+        src={logo}
+        />
+        <div className="flex flex-col justify-center items-center pr-8">
+          <div className="relative ml-7">
+            <input
+              className="w-full h-10 pl-4 pr-10 text-sm bg-white border-none rounded-full shadow-sm sm:w-56"
+              id="search"
+              type="search"
+              placeholder="Título, director o año"
+              value={search}
+              onChange={(e) => searchHandler(e)}
+            />
+            <button
+              className="absolute p-2 text-gray-600 rounded-full transition -translate-y-1/2 hover:text-gray-700 bg-[#c1cba4] top-1/2 right-1 hover:border-transparentr"
+              type="button"
+              aria-label="Submit Search"
+              disabled={true}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 h-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      {listFilms.length != 0 ?
+        <div className="flex flex-wrap justify-center gap-6 pt-6 pb-6 w-full mt-[97px]">
+        {listFilms.map((film) => (
+          <Card
+            key={film.id}
+            film={film}
+          />
+          ))}
+        </div>
+        :
+        <NotFound />
+      }
+    </>
+  )
+}
